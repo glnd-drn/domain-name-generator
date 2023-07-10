23720b3a12a85d826a149093e1a09a711e86f73e
Glenda Durán <130619011+glnd-drn@users.noreply.github.com>
2023-07-10 21:29:01 +0000

    Cambios1

--- src/app.js
+++ src/app.js
@@ -1,25 +1,36 @@
 let pronombres = ["yo", "vos", "ustedes"];
 let adjetivos = ["grande", "corto", "preciosa"];
-let sustantivos = ["amor", "dominó", "playa"];
-let acciones = ["bailó", "escuchó", "comió"];
+let sustantivos = ["amor", "domino", "playa"];
+let acciones = ["bailo", "escucho", "comio"];
+let extensiones = [".com", ".net", ".es"];
 
-function generarNombresDominio(pronombres, adjetivos, sustantivos, acciones) {
+function generarNombresDominio(pronombres, adjetivos, sustantivos, acciones, extensiones) {
   let nombresDominio = [];
   let conteo = 0;
   pronombres.forEach((pronombre) => {
     adjetivos.forEach((adjetivo) => {
       sustantivos.forEach((sustantivo) => {
         acciones.forEach((accion) => {
-          let nombreDominio = pronombre + adjetivo + sustantivo + accion;
+          extensiones.forEach((extensiones) => {
+          let nombreDominio = pronombre + adjetivo + sustantivo + accion + extensiones;
           nombresDominio.push(nombreDominio);
           conteo++;
+          });
         });
       });
     });
   });
-console.log(conteo);
+
+
+  console.log(conteo);
   return nombresDominio;
 }
 
-let nombresDominio = generarNombresDominio(pronombres, adjetivos, sustantivos, acciones);
+let nombresDominio = generarNombresDominio(
+  pronombres,
+  adjetivos,
+  sustantivos,
+  acciones, 
+  extensiones
+);
 console.log(nombresDominio);
